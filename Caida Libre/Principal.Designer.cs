619271ae9e1213c4f05d3c12d438e8666d2bb689
ziroz <ziroz155@gmail.com>
2015-11-20 23:01:16 -0500

    campos dinamicos para los filtros

--- Caida Libre/Principal.Designer.cs
+++ Caida Libre/Principal.Designer.cs
@@ -31,10 +31,9 @@
             this.cmdIncognitas = new System.Windows.Forms.ComboBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.rdVfInicalTiempo = new System.Windows.Forms.RadioButton();
-            this.rdVfInicialAltura = new System.Windows.Forms.RadioButton();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.tblDatos = new System.Windows.Forms.TableLayoutPanel();
+            this.tblOpciones = new System.Windows.Forms.TableLayoutPanel();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -56,14 +55,13 @@
             this.cmdIncognitas.Name = "cmdIncognitas";
             this.cmdIncognitas.Size = new System.Drawing.Size(153, 21);
             this.cmdIncognitas.TabIndex = 0;
-            this.cmdIncognitas.SelectedIndexChanged += new System.EventHandler(this.cmdIncognitas_SelectedIndexChanged);
             // 
             // groupBox1
             // 
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.Controls.Add(this.cmdIncognitas);
-            this.groupBox1.Location = new System.Drawing.Point(59, 41);
+            this.groupBox1.Location = new System.Drawing.Point(55, 41);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(330, 77);
             this.groupBox1.TabIndex = 1;
@@ -74,46 +72,22 @@
             // 
             this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox2.Controls.Add(this.rdVfInicalTiempo);
-            this.groupBox2.Controls.Add(this.rdVfInicialAltura);
-            this.groupBox2.Location = new System.Drawing.Point(59, 151);
+            this.groupBox2.Controls.Add(this.tblOpciones);
+            this.groupBox2.Location = new System.Drawing.Point(55, 151);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(330, 71);
             this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Velocidad Final";
             // 
-            // rdVfInicalTiempo
-            // 
-            this.rdVfInicalTiempo.AutoSize = true;
-            this.rdVfInicalTiempo.Location = new System.Drawing.Point(198, 33);
-            this.rdVfInicalTiempo.Name = "rdVfInicalTiempo";
-            this.rdVfInicalTiempo.Size = new System.Drawing.Size(117, 17);
-            this.rdVfInicalTiempo.TabIndex = 1;
-            this.rdVfInicalTiempo.TabStop = true;
-            this.rdVfInicalTiempo.Text = "Vel. Inicial - Tiempo";
-            this.rdVfInicalTiempo.UseVisualStyleBackColor = true;
-            // 
-            // rdVfInicialAltura
-            // 
-            this.rdVfInicialAltura.AutoSize = true;
-            this.rdVfInicialAltura.Location = new System.Drawing.Point(36, 33);
-            this.rdVfInicialAltura.Name = "rdVfInicialAltura";
-            this.rdVfInicialAltura.Size = new System.Drawing.Size(109, 17);
-            this.rdVfInicialAltura.TabIndex = 0;
-            this.rdVfInicialAltura.TabStop = true;
-            this.rdVfInicialAltura.Text = "Vel. Inicial - Altura";
-            this.rdVfInicialAltura.UseVisualStyleBackColor = true;
-            this.rdVfInicialAltura.CheckedChanged += new System.EventHandler(this.IncognitaVelocidadFinal_CheckedChanged);
-            // 
             // groupBox3
             // 
             this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox3.Controls.Add(this.tblDatos);
-            this.groupBox3.Location = new System.Drawing.Point(59, 247);
+            this.groupBox3.Location = new System.Drawing.Point(55, 247);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(330, 194);
+            this.groupBox3.Size = new System.Drawing.Size(330, 149);
             this.groupBox3.TabIndex = 3;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Datos";
@@ -123,12 +97,26 @@
             this.tblDatos.ColumnCount = 2;
             this.tblDatos.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tblDatos.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tblDatos.Location = new System.Drawing.Point(18, 32);
+            this.tblDatos.Location = new System.Drawing.Point(26, 32);
             this.tblDatos.Name = "tblDatos";
             this.tblDatos.RowCount = 1;
-            this.tblDatos.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 128F));
-            this.tblDatos.Size = new System.Drawing.Size(289, 137);
+            this.tblDatos.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 137F));
+            this.tblDatos.Size = new System.Drawing.Size(278, 91);
             this.tblDatos.TabIndex = 0;
+            // 
+            // tblOpciones
+            // 
+            this.tblOpciones.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tblOpciones.ColumnCount = 2;
+            this.tblOpciones.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblOpciones.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblOpciones.Location = new System.Drawing.Point(26, 23);
+            this.tblOpciones.Name = "tblOpciones";
+            this.tblOpciones.RowCount = 1;
+            this.tblOpciones.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tblOpciones.Size = new System.Drawing.Size(278, 30);
+            this.tblOpciones.TabIndex = 0;
             // 
             // Principal
             // 
@@ -143,7 +131,6 @@
             this.Text = "Datos";
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
             this.groupBox3.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -154,10 +141,9 @@
         private System.Windows.Forms.ComboBox cmdIncognitas;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.RadioButton rdVfInicalTiempo;
-        private System.Windows.Forms.RadioButton rdVfInicialAltura;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.TableLayoutPanel tblDatos;
+        private System.Windows.Forms.TableLayoutPanel tblOpciones;
     }
 }
 
--- Caida Libre/Principal.cs
+++ Caida Libre/Principal.cs
@@ -13,6 +13,7 @@ namespace Caida_Libre
     public partial class Principal : Form
     {
         Dictionary<string, List<Control>> controles = new Dictionary<string, List<Control>>();
+        Dictionary<string, List<Control>> opciones = new Dictionary<string, List<Control>>();
 
         Dictionary<string, string> incognitas = new Dictionary<string, string>()
         {
@@ -26,12 +27,14 @@ namespace Caida_Libre
         {
             InitializeComponent();
 
+            ConfigurarOpciones();
+
             cmdIncognitas.DataSource = incognitas.Select(m => new { key = m.Key, value = m.Value }).ToList();
             cmdIncognitas.ValueMember = "key";
             cmdIncognitas.DisplayMember = "value";
+            cmdIncognitas.Text = "Seleccione";
 
-
-            ConfigurarOpciones();
+            cmdIncognitas.SelectedIndexChanged += cmdIncognitas_SelectedIndexChanged;
         }
 
         private void ConfigurarOpciones()
@@ -93,42 +96,95 @@ namespace Caida_Libre
                 new TextBox(){ Name = "txtAltura" },
             });
 
+            //Velocidad Final 
+            opciones.Add("Vf", new List<Control>()
+            {
+                new RadioButton(){ Name = "Vf1", Text = "Vel. Inicial - Altura", Checked=true, Anchor = AnchorStyles.Left | AnchorStyles.Right },
+                new RadioButton(){ Name = "Vf2", Text = "Vel. Inicial - Tiempo", Anchor = AnchorStyles.Left | AnchorStyles.Right },
+            });
+
+
+            opciones.Add("t", new List<Control>()
+            {
+                new RadioButton(){ Name = "t1", Text = "Vel. Inicial - Altura", Checked=true, Anchor = AnchorStyles.Left | AnchorStyles.Right},
+                new RadioButton(){ Name = "t2", Text = "Vel. Inicial - Vel. Final", Anchor = AnchorStyles.Left | AnchorStyles.Right},
+            });
+
+
+            opciones.Add("h", new List<Control>()
+            {
+                new RadioButton(){ Name = "h1", Text = "Vel. Inicial - Vel. Final", Checked=true, Anchor = AnchorStyles.Left| AnchorStyles.Right },
+            });
+
+            opciones.Add("Vo", new List<Control>()
+            {
+                new RadioButton(){ Name = "Vo1", Text = "Altura", Checked=true, Anchor = AnchorStyles.Left | AnchorStyles.Right},
+            });
         }
 
-        private void IncognitaVelocidadFinal_CheckedChanged(object sender, EventArgs e)
+        private void CheckedChange(object sender, EventArgs e)
         {
             tblDatos.RowStyles.Clear();
-            string filtro = rdVfInicialAltura.Checked ? "1" : "2";
 
-            var campos = controles["Vf" + filtro];
+            string filtro = "";
 
-            for(int i = 0; i< campos.Count; i++)
+            foreach (var control in tblOpciones.Controls)
             {
-                var elem = campos[i];
+                var elem = control as RadioButton;
 
-                if (elem.GetType() == typeof(TextBox))
+                if (elem.Checked)
                 {
-                    var valorActual = tblDatos.Controls.Find(elem.Name, true).FirstOrDefault();
-
-                    if (valorActual != null)
-                    {
-                        elem.Text = valorActual.Text;
-                    }
+                    filtro = elem.Name;
                 }
             }
-            
-            tblDatos.Controls.Clear();
 
-            foreach (var elem in campos)
+            if (!string.IsNullOrEmpty(filtro))
             {
-                tblDatos.Controls.Add(elem);
-            }
+                var campos = controles[filtro];
 
+                for (int i = 0; i < campos.Count; i++)
+                {
+                    var elem = campos[i];
+
+                    if (elem.GetType() == typeof(TextBox))
+                    {
+                        var valorActual = tblDatos.Controls.Find(elem.Name, true).FirstOrDefault();
+
+                        if (valorActual != null)
+                        {
+                            elem.Text = valorActual.Text;
+                        }
+                    }
+                }
+
+                tblDatos.Controls.Clear();
+
+                foreach (var elem in campos)
+                {
+                    tblDatos.Controls.Add(elem);
+                }
+            }
         }
 
         private void cmdIncognitas_SelectedIndexChanged(object sender, EventArgs e)
         {
+            var filtro = sender as ComboBox;
 
+            tblOpciones.RowStyles.Clear();
+            tblOpciones.Controls.Clear();
+
+            if (filtro.SelectedValue != null)
+            {
+                var campos = opciones[filtro.SelectedValue.ToString()];
+
+                foreach (var elem in campos)
+                {
+                    tblOpciones.Controls.Add(elem);
+                    ((RadioButton)tblOpciones.Controls.Find(elem.Name, true)[0]).CheckedChanged += CheckedChange;
+                }
+            }
+
+            CheckedChange(null, null);
         }
     }
 }
